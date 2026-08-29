ed9a26b51aef5ac3ef15e9d14a6cf271835b2a7b
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2026-08-29 10:00:16 +0800

    FFM version of native function call

--- src/main/java/io/github/lumine1909/imelangrestorer/util/WindowsImeUtil.java
+++ src/main/java/io/github/lumine1909/imelangrestorer/util/WindowsImeUtil.java
@@ -1,132 +1,128 @@
 package io.github.lumine1909.imelangrestorer.util;
 
 import org.lwjgl.glfw.GLFWNativeWin32;
-import org.lwjgl.system.JNI;
-import org.lwjgl.system.MemoryStack;
 import org.lwjgl.system.Platform;
-import org.lwjgl.system.SharedLibrary;
 
-import java.nio.IntBuffer;
-
-import static org.lwjgl.system.Checks.CHECKS;
-import static org.lwjgl.system.Checks.check;
-import static org.lwjgl.system.Library.loadNative;
-import static org.lwjgl.system.MemoryUtil.memAddress;
+import java.lang.foreign.Arena;
+import java.lang.foreign.FunctionDescriptor;
+import java.lang.foreign.Linker;
+import java.lang.foreign.MemorySegment;
+import java.lang.foreign.SymbolLookup;
+import java.lang.foreign.ValueLayout;
+import java.lang.invoke.MethodHandle;
 
 public final class WindowsImeUtil {
 
-    private static final SharedLibrary IMM32;
-
-    private static final long IMM_GET_CONTEXT;
-    private static final long IMM_RELEASE_CONTEXT;
-    private static final long IMM_GET_CONVERSION_STATUS;
-    private static final long IMM_SET_CONVERSION_STATUS;
-
     private static final int IME_CMODE_NATIVE = 1024 | 1;
+
+    private static final Linker LINKER = Linker.nativeLinker();
+    private static final SymbolLookup IMM32;
+
+    private static final MethodHandle IMM_GET_CONTEXT;
+    private static final MethodHandle IMM_RELEASE_CONTEXT;
+    private static final MethodHandle IMM_GET_CONVERSION_STATUS;
+    private static final MethodHandle IMM_SET_CONVERSION_STATUS;
 
     static {
         if (Platform.get() == Platform.WINDOWS) {
-            IMM32 = loadNative("imm32", "imm32");
-            IMM_GET_CONTEXT = IMM32.getFunctionAddress("ImmGetContext");
-            IMM_RELEASE_CONTEXT = IMM32.getFunctionAddress("ImmReleaseContext");
-            IMM_GET_CONVERSION_STATUS = IMM32.getFunctionAddress("ImmGetConversionStatus");
-            IMM_SET_CONVERSION_STATUS = IMM32.getFunctionAddress("ImmSetConversionStatus");
+            IMM32 = SymbolLookup.libraryLookup("imm32", Arena.ofShared());
+            IMM_GET_CONTEXT = LINKER.downcallHandle(
+                IMM32.findOrThrow("ImmGetContext"),
+                FunctionDescriptor.of(ValueLayout.ADDRESS, ValueLayout.ADDRESS)
+            );
+            IMM_RELEASE_CONTEXT = LINKER.downcallHandle(
+                IMM32.findOrThrow("ImmReleaseContext"),
+                FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.ADDRESS)
+            );
+            IMM_GET_CONVERSION_STATUS = LINKER.downcallHandle(
+                IMM32.findOrThrow("ImmGetConversionStatus"),
+                FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.ADDRESS, ValueLayout.ADDRESS)
+            );
+            IMM_SET_CONVERSION_STATUS = LINKER.downcallHandle(
+                IMM32.findOrThrow("ImmSetConversionStatus"),
+                FunctionDescriptor.of(ValueLayout.JAVA_INT, ValueLayout.ADDRESS, ValueLayout.JAVA_INT, ValueLayout.JAVA_INT)
+            );
         } else {
             IMM32 = null;
-            IMM_GET_CONTEXT = 0L;
-            IMM_RELEASE_CONTEXT = 0L;
-            IMM_GET_CONVERSION_STATUS = 0L;
-            IMM_SET_CONVERSION_STATUS = 0L;
+            IMM_GET_CONTEXT = null;
+            IMM_RELEASE_CONTEXT = null;
+            IMM_GET_CONVERSION_STATUS = null;
+            IMM_SET_CONVERSION_STATUS = null;
         }
     }
 
     private WindowsImeUtil() {
     }
 
-    public static boolean isNative(final long glfwWindow) {
+    public static boolean isNative(long glfwWindow) {
         if (IMM32 == null) {
             return false;
         }
 
-        final long hwnd = GLFWNativeWin32.glfwGetWin32Window(glfwWindow);
+        long hwnd = GLFWNativeWin32.glfwGetWin32Window(glfwWindow);
         if (hwnd == 0L) {
             return false;
         }
 
-        if (CHECKS) {
-            check(IMM_GET_CONTEXT);
-            check(IMM_GET_CONVERSION_STATUS);
-            check(IMM_RELEASE_CONTEXT);
-        }
-
-        // HIMC ImmGetContext(HWND hWnd)
-        final long himc = JNI.callPP(hwnd, IMM_GET_CONTEXT);
-        if (himc == 0L) {
-            return false;
-        }
-
-        try (MemoryStack stack = MemoryStack.stackPush()) {
-            IntBuffer conversion = stack.mallocInt(1);
-            IntBuffer sentence = stack.mallocInt(1);
-
-            // BOOL ImmGetConversionStatus(HIMC hIMC, LPDWORD lpfdwConversion, LPDWORD lpfdwSentence);
-            int result = JNI.callPPPI(himc, memAddress(conversion), memAddress(sentence), IMM_GET_CONVERSION_STATUS);
-            if (result == 0) {
-                return false;
+        try (Arena arena = Arena.ofConfined()) {
+            MemorySegment hwndSegment = MemorySegment.ofAddress(hwnd).reinterpret(0);
+            MemorySegment himc = (MemorySegment) IMM_GET_CONTEXT.invokeExact(hwndSegment);
+            try {
+                if (himc.equals(MemorySegment.NULL)) {
+                    return false;
+                }
+                MemorySegment conversion = arena.allocate(ValueLayout.JAVA_INT);
+                MemorySegment sentence = arena.allocate(ValueLayout.JAVA_INT);
+                int result = (int) IMM_GET_CONVERSION_STATUS.invokeExact(himc, conversion, sentence);
+                if (result == 0) {
+                    return false;
+                }
+                int conversionMode = conversion.get(ValueLayout.JAVA_INT, 0);
+                return (conversionMode & IME_CMODE_NATIVE) != 0;
+            } finally {
+                int _ = (int) IMM_RELEASE_CONTEXT.invokeExact(hwndSegment, himc);
             }
-            return (conversion.get(0) & IME_CMODE_NATIVE) != 0;
-        } finally {
-            // BOOL ImmReleaseContext(HWND hWnd, HIMC hIMC)
-            JNI.callPPI(hwnd, himc, IMM_RELEASE_CONTEXT);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public static void setNative(final long glfwWindow, final boolean nativeMode) {
+    public static void setNative(long glfwWindow, boolean nativeMode) {
         if (IMM32 == null) {
             return;
         }
 
-        final long hwnd = GLFWNativeWin32.glfwGetWin32Window(glfwWindow);
+        long hwnd = GLFWNativeWin32.glfwGetWin32Window(glfwWindow);
         if (hwnd == 0L) {
             return;
         }
 
-        if (CHECKS) {
-            check(IMM_GET_CONTEXT);
-            check(IMM_GET_CONVERSION_STATUS);
-            check(IMM_SET_CONVERSION_STATUS);
-            check(IMM_RELEASE_CONTEXT);
-        }
-
-        // HIMC ImmGetContext(HWND hWnd)
-        final long himc = JNI.callPP(hwnd, IMM_GET_CONTEXT);
-        if (himc == 0L) {
-            return;
-        }
-
-        try (MemoryStack stack = MemoryStack.stackPush()) {
-            IntBuffer conversion = stack.mallocInt(1);
-            IntBuffer sentence = stack.mallocInt(1);
-
-            // BOOL ImmGetConversionStatus(HIMC hIMC, LPDWORD lpfdwConversion, LPDWORD lpfdwSentence);
-            int result = JNI.callPPPI(himc, memAddress(conversion), memAddress(sentence), IMM_GET_CONVERSION_STATUS);
-            if (result == 0) {
-                return;
+        try (Arena arena = Arena.ofConfined()) {
+            MemorySegment hwndSegment = MemorySegment.ofAddress(hwnd).reinterpret(0);
+            MemorySegment himc = (MemorySegment) IMM_GET_CONTEXT.invokeExact(hwndSegment);
+            try {
+                if (himc.equals(MemorySegment.NULL)) {
+                    return;
+                }
+                MemorySegment conversion = arena.allocate(ValueLayout.JAVA_INT);
+                MemorySegment sentence = arena.allocate(ValueLayout.JAVA_INT);
+                int result = (int) IMM_GET_CONVERSION_STATUS.invokeExact(himc, conversion, sentence);
+                if (result == 0) {
+                    return;
+                }
+                int conversionMode = conversion.get(ValueLayout.JAVA_INT, 0);
+                int sentenceMode = sentence.get(ValueLayout.JAVA_INT, 0);
+                if (nativeMode) {
+                    conversionMode |= IME_CMODE_NATIVE;
+                } else {
+                    conversionMode &= ~IME_CMODE_NATIVE;
+                }
+                int _ = (int) IMM_SET_CONVERSION_STATUS.invokeExact(himc, conversionMode, sentenceMode);
+            } finally {
+                int _ = (int) IMM_RELEASE_CONTEXT.invokeExact(hwndSegment, himc);
             }
-
-            int conversionMode = conversion.get(0);
-            int sentenceMode = sentence.get(0);
-            if (nativeMode) {
-                conversionMode |= IME_CMODE_NATIVE;
-            } else {
-                conversionMode &= ~IME_CMODE_NATIVE;
-            }
-
-            // BOOL ImmSetConversionStatus(HIMC hIMC, DWORD fdwConversion, DWORD fdwSentence);
-            JNI.callPPPI(himc, conversionMode, sentenceMode, IMM_SET_CONVERSION_STATUS);
-        } finally {
-            // BOOL ImmReleaseContext(HWND hWnd, HIMC hIMC)
-            JNI.callPPI(hwnd, himc, IMM_RELEASE_CONTEXT);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
         }
     }
 }
